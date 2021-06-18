2c07681cd0e1c0b836c22d701208b3b59d6ca27e
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2021-06-18 15:33:44 +0100

    feat: inital commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Fliqpay API
+
+This is a simple proxy server to add an API to requests before forwarding them to the [fixer api](fixer.io). The api runs on heroku here: [https://cirqlar-fliqpay.herokuapp.com/](https://cirqlar-fliqpay.herokuapp.com/)
+
+## Installation
+
+### Clone the source code 
+```bash
+git clone https://github.com/cirqlar/fliqpay-api.git
+```
+### Install dependencies
+_Note: Ensure you have changed into the cloned directory before you run this command_
+```bash
+npm install
+```
+
+### Start development server
+
+Replace `YOUR_API_KEY` with your [fixer](fixer.io) api key. Other environment variables you can set are `PORT` (the port the server will run on. Defaults to `8000`) and `CORS_HOSTS` (a comma separated list of origins that should be allowed through CORS, eg: `"http://locahost:3000, https://cirqlar.github.io"`. __NB: The space after the comma is important__. Defaults to `"http://localhost:3000"`)
+```bash
+API_KEY=YOUR_API_KEY npm start
+```
+You can now view the app on [http://localhost:8000](http://localhost:8000). (change port if you set a different one);
---
+++ index.js
@@ -0,0 +1,55 @@
+const app = require("express")();
+const cors = require("cors");
+const proxy = require("express-http-proxy");
+
+// Destructure env variables with sensible defaults
+const { PORT = 8000, CORS_HOSTS = "http://localhost:3000", API_KEY } = process.env;
+
+// Redirect to https in production
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === "production") {
+    if (req.headers["x-forwarded-proto"] !== "https") {
+      return res.redirect("https://" + req.headers.host + req.url);
+    }
+    else {
+      return next();
+    }
+  } else {
+    return next();
+  }
+});
+
+// Only allow Cross Origin Requests from select hosts
+const hosts = CORS_HOSTS.split(", ");
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (hosts.indexOf(origin) === -1) {
+      var msg = "The CORS policy for this site does not " + "allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+
+    return callback(null, true);
+  },
+};
+app.use(cors(corsOptions));
+
+// Proxy requests to github api
+app.use(
+  "/",
+  proxy("http://data.fixer.io/", {
+    proxyReqPathResolver: function (req) {
+      return (
+        "/api" +
+        req.path +
+        `?access_key=${API_KEY}` +
+        Object.entries(req.query)
+          .map(([key, value]) => `&${key}=${value}`)
+          .join("")
+      );
+    },
+  })
+);
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
